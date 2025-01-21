a8998f8c36f783566c8835b2d27568decda2dcc2
Ilya <ul7309@mail.ru>
2025-01-21 13:31:10 +0400

    Разнесение логики и базовая стилизация

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterOutlet, RouterLink } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
+
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, SidebarComponent, MatSidenavModule, MatToolbarModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'management-service';
 }
---
+++ src/app/components/sidebar/sidebar.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+
+import { MatListModule } from '@angular/material/list';
+
+@Component({
+  selector: 'app-sidebar',
+  imports: [CommonModule, RouterLink, MatListModule],
+  templateUrl: './sidebar.component.html',
+  styleUrl: './sidebar.component.scss'
+})
+export class SidebarComponent {
+  navigationMenuList = [
+    {
+      link: '',
+      title: 'Главная',
+    },
+    {
+      link: '/employee',
+      title: 'Сотрудник',
+    },
+  ];
+}
