effedeb06664c282cd8c2c0f7725f05697714603
Ilya <ul7309@mail.ru>
2025-01-21 16:41:27 +0400

    Карточка сотрудника

--- src/app/pages/Employee/employee.component.ts
+++ src/app/pages/Employee/employee.component.ts
@@ -1,15 +1,50 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, FormGroup, FormControl, Validators, ReactiveFormsModule} from '@angular/forms';
+
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-employee',
+  imports: [FormsModule, ReactiveFormsModule, CommonModule, MatInputModule, MatFormFieldModule, MatButtonModule, MatCardModule],
   templateUrl: './employee.component.html',
-  styleUrls: ['./employee.component.scss'], // или .css
-  standalone: true // Указываем, что компонент является самостоятельным
+  styleUrls: ['./employee.component.scss'],
+  standalone: true,
 })
+
 export class EmployeeComponent {
-  employeeName: string = 'John Doe';
+  myForm: FormGroup;
+
+  fields = [
+    { key: 'fio', label: 'ФИО', validators: [Validators.required] },
+    { key: 'department', label: 'Отдел', validators: [Validators.required] },
+    { key: 'mainInformation', label: 'Главная информация', validators: [Validators.required] },
+    { key: 'education', label: 'Образование', validators: [Validators.required] },
+    { key: 'grade', label: 'Грейд', validators: [Validators.required] },
+    { key: 'location', label: 'Локация', validators: [Validators.required] },
+    { key: 'englishLevel', label: 'Уровень английского', validators: [Validators.required] },
+    { key: 'specialization', label: 'Специализация', validators: [Validators.required] },
+    { key: 'coverLetter', label: 'Сопроводительное письмо', validators: [Validators.required] },
+    { key: 'supervisor', label: 'Руководитель', validators: [Validators.required] },
+  ];
 
   constructor() {
-    // код инициализации
+    this.myForm = new FormGroup({});
+
+    this.fields.forEach(field => {
+      this.myForm.addControl(field.key, new FormControl('', field.validators));
+    });
+  }
+
+  hasError(controlName: string): boolean {
+    const control = this.myForm.controls[controlName];
+    return control.invalid && control.touched;
+  }
+
+  submit() {
+    console.log(this.myForm);
   }
 }
