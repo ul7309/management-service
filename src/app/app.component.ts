603156fcf33f25708794314c29563415ddb33578
Ilya <ul7309@mail.ru>
2025-01-30 21:52:49 +0400

    Верстка главной страницы

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,10 +5,11 @@ import { SidebarComponent } from './layout/sidebar/sidebar.component';
 
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SidebarComponent, MatSidenavModule, MatToolbarModule],
+  imports: [RouterOutlet, SidebarComponent, MatSidenavModule, MatToolbarModule, MatButton],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
