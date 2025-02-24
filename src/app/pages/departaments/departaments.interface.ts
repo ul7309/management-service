773a303968131b74f6bd8d4095979055655ce139
Ilya <ul7309@mail.ru>
2025-02-24 11:07:51 +0400

    Страница отделов

--- src/app/pages/departaments/departaments.interface.ts
+++ src/app/pages/departaments/departaments.interface.ts
@@ -1,8 +1,14 @@
 import { SimpleEmployee } from '../employee/employee.interface';
 
+export interface Item {
+  id: string;
+  name: string;
+}
+
 export interface Departaments {
   id: string;
   name: string;
+  items: Item[];
 }
 
 export interface Departament {
