a3059c1a1ae4d8845f29d8b9bf8c80b5048ea7fa
Ilya <ul7309@mail.ru>
2025-02-27 17:21:04 +0400

    Страница добавления сотрудника

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 
 import { EmployeesComponent } from './pages/employees/employees.component';
 import { EmployeeComponent } from './pages/employee/employee.component';
+import { AddEmployeeComponent } from './pages/add-employee/add-employee.component';
 import { DepartamentsComponent } from './pages/departaments/departaments.component';
 import { DepartamentComponent } from './pages/departament/departament.component';
 
 export const routes: Routes = [
   { path: 'employees', component: EmployeesComponent },
+  { path: 'employee/add', component: AddEmployeeComponent },
   { path: 'employee/:id', component: EmployeeComponent },
   { path: 'departaments', component: DepartamentsComponent },
   { path: 'departament/:id', component: DepartamentComponent },
