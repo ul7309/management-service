57b1374fa3394a5aba9729fc721a2f50f41d9c75
Ilya <ul7309@mail.ru>
2025-02-28 15:02:40 +0400

    Форма добавления отдела

--- src/app/pages/add-departament/add-departament.component.ts
+++ src/app/pages/add-departament/add-departament.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
+
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
 
 import { LayoutComponent } from '../../layout/layout.component';
 
 @Component({
   selector: 'app-add-departament',
-  imports: [LayoutComponent],
+  imports: [ReactiveFormsModule, ButtonModule, InputTextModule, LayoutComponent],
   templateUrl: './add-departament.component.html',
   styleUrl: './add-departament.component.scss'
 })
 
 export class AddDepartamentComponent {
+  departamentForm = new FormGroup({
+    departament: new FormControl(''),
+  });
+
+  submit() {
+    console.log('submit');
+  }
 }
